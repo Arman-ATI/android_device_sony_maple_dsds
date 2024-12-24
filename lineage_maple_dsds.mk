77879ecaf2135529cd894e61b6a87225fa56f1bb
Arman-ATI <altafiarman5@gmail.com>
2024-12-24 14:00:32 +0000

    maple_dsds: init rising

--- lineage_maple_dsds.mk
+++ lineage_maple_dsds.mk
@@ -15,6 +15,31 @@ TARGET_BOOTANIMATION_HALF_RES := true
 ### LINEAGE
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Rising
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon 835" \
+    RisingMaintainer="Arman ATI"
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# Launchers
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# Extra stuff
+TARGET_PREBUILT_BCR := true
+
+# Camera
+TARGET_PREBUILT_GOOGLE_CAMERA := true
+
+# Gapps
+WITH_GMS := true
+
+# ADB root
+ALLOW_ADBD_ROOT := true
+ALLOW_LOCAL_PROP_OVERRIDE := true
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_maple_dsds
 PRODUCT_DEVICE := maple_dsds
